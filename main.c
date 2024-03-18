512a7ff6a6e252b9ec0e7fa707c035a23963d6a3
hassanmagueri <hassanmagueri2001@gmail.com>
2024-03-18 15:32:28 +0000

    fr

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include <unistd.h>
 
 int main() {
-    char new_path[100]; // Assuming maximum path length is 100 characters
+    char new_path[20]; // Assuming maximum path length is 100 characters
     
-    printf("Enter the new path: ");
+    printf("new path: ");
     scanf("%s", new_path);
     
     // Change directory to the new path
     if (chdir(new_path) == 0) {
         printf("Directory changed to: %s\n", new_path);
     } else {
-        printf("Failed to change directory to: %s\n", new_path);
+        printf("change directory to: %s\n", new_path);
     }
     
     return 0;
